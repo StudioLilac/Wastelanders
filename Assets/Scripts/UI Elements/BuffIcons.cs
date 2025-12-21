89cfd61b298b02f8d2a8e2603e69eb084e21415d
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-12-21 14:14:45 -0800

    Clean up bufficons

--- Assets/Scripts/UI Elements/BuffIcons.cs
+++ Assets/Scripts/UI Elements/BuffIcons.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -15,12 +14,6 @@ public class BuffIcons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     public delegate void BuffIconHoveredHandler(string buffName, int stacks, bool hovered);
     public static event BuffIconHoveredHandler OnBuffIconHovered;
 
-    private void Start()
-    {
-        RectTransform rt = GetComponent<RectTransform>();
-        rt.pivot = new Vector2(0.5f, 0.5f);
-    }
-
     public void SetText(string text)
     {
         textMeshProUGUI.text = text;
@@ -31,21 +24,8 @@ public class BuffIcons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
         buffIcon.sprite = icon;
     }
 
-    // potential issue with several bufficons invoking the same static event...
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log($"Pointer world pos: {eventData.pointerCurrentRaycast.worldPosition}");
-        Debug.Log($"BuffIcon actual position: {transform.position}");
-        Debug.Log($"BuffIcon RectTransform world corners:");
-    
-        RectTransform rt = GetComponent<RectTransform>();
-        Vector3[] corners = new Vector3[4];
-        rt.GetWorldCorners(corners);
-        for (int i = 0; i < 4; i++)
-        {
-            Debug.Log($"  Corner {i}: {corners[i]}");
-        }
-    
         OnBuffIconHovered?.Invoke(buffName, stacks, hovered: true);
     }
 
