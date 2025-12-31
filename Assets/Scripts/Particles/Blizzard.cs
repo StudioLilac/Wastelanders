16c81c6d916cfa46dda53df5fc653d67a2470558
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-12-31 14:27:17 -0800

    Code cleanup

--- Assets/Scripts/Particles/Blizzard.cs
+++ Assets/Scripts/Particles/Blizzard.cs
@@ -6,29 +6,8 @@ namespace Particles {
     /// Snow is affected more by wind, tumbles as it falls, and has a longer lifetime than rain.
     /// Attach this to a GameObject with a ParticleSystem component.
     /// </summary>
-    [RequireComponent(typeof(ParticleSystem))]
-    public class Blizzard : MonoBehaviour {
+    public class Blizzard : Particles {
         [Header("Snow Settings")]
-        [Tooltip("Number of snow particles emitted per second")]
-        [SerializeField]
-        private float emissionRate = 50f;
-
-        [Tooltip("Lifetime of each snowflake in seconds (longer than rain)")]
-        [SerializeField]
-        private float lifetime = 4f;
-
-        [Tooltip("Base fall speed of snow (slower than rain)")]
-        [SerializeField]
-        private float fallSpeed = 3f;
-
-        [Tooltip("Width of the snow spawn area")]
-        [SerializeField]
-        private float spawnWidth = 50f;
-
-        [Tooltip("Height of the snow spawn area")]
-        [SerializeField]
-        private float spawnHeight = 3f;
-
         [Tooltip("Size of snowflakes")]
         [SerializeField]
         private float flakeSize = 0.2f;
@@ -42,10 +21,6 @@ namespace Particles {
         private Color snowColor = new Color(1f, 1f, 1f, 0.6f);
 
         [Header("Wind Settings")]
-        [Tooltip("Horizontal wind force affecting snow (snow is very affected by wind)")]
-        [SerializeField]
-        private float windStrength = 0f;
-
         [Tooltip("Wind is more pronounced on snow - multiplier for wind effect")]
         [SerializeField]
         private float windInfluence = 2f;
@@ -76,31 +51,17 @@ namespace Particles {
         [SerializeField]
         private float rotationSpeed = 180f;
 
-        [Header("Intensity")]
-        [Range(0f, 1f)]
-        [Tooltip("Overall snow intensity (0 = no snow, 1 = heavy blizzard)")]
-        [SerializeField]
-        private float intensity = 1f;
-
-        private ParticleSystem snowParticleSystem;
-        private ParticleSystem.MainModule mainModule;
-        private ParticleSystem.EmissionModule emissionModule;
-        private ParticleSystem.ShapeModule shapeModule;
-        private ParticleSystem.VelocityOverLifetimeModule velocityModule;
         private ParticleSystem.SizeOverLifetimeModule sizeModule;
         private ParticleSystem.RotationOverLifetimeModule rotationModule;
 
-        private float baseEmissionRate;
         private float windTimer;
 
-        private void Awake() {
-            snowParticleSystem = GetComponent<ParticleSystem>();
-            InitializeParticleSystem();
+        protected override void Awake() {
+            base.Awake();
             StartSnow();
         }
 
         private void Update() {
-            // Simulate wind gusts
             if (windGustAmount > 0f) {
                 windTimer += Time.deltaTime * windGustSpeed;
                 float gustWind = windStrength + Mathf.Sin(windTimer) * windGustAmount;
@@ -108,39 +69,21 @@ namespace Particles {
             }
         }
 
-        private void InitializeParticleSystem() {
-            // Cache modules
-            mainModule = snowParticleSystem.main;
-            emissionModule = snowParticleSystem.emission;
-            shapeModule = snowParticleSystem.shape;
-            velocityModule = snowParticleSystem.velocityOverLifetime;
-            sizeModule = snowParticleSystem.sizeOverLifetime;
-            rotationModule = snowParticleSystem.rotationOverLifetime;
+        protected override void InitializeParticleSystem() {
+            base.InitializeParticleSystem();
 
-            // Configure main module
-            mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
-            mainModule.startLifetime = lifetime;
-            mainModule.startSpeed = 0f; // We control speed via velocity over lifetime
-            // Use size range for variation
+            // Cache additional modules
+            sizeModule = particleSystem.sizeOverLifetime;
+            rotationModule = particleSystem.rotationOverLifetime;
+
+            // Configure main module specifics
             mainModule.startSize = new ParticleSystem.MinMaxCurve(flakeSize * (1f - flakeSizeVariation), flakeSize * (1f + flakeSizeVariation));
             mainModule.startColor = snowColor;
             mainModule.gravityModifier = 0.1f; // Slight gravity for natural settling
             mainModule.maxParticles = 5000;
 
-            // Configure emission
-            baseEmissionRate = emissionRate;
-            emissionModule.rateOverTime = emissionRate * intensity;
-
-            // Configure shape (box emitter above the scene)
-            shapeModule.shapeType = ParticleSystemShapeType.Box;
-            shapeModule.scale = new Vector3(spawnWidth, spawnHeight, 1f);
-
-            // Configure velocity for falling snow
-            velocityModule.enabled = true;
-            velocityModule.space = ParticleSystemSimulationSpace.World;
+            // Configure velocity with wind influence
             velocityModule.x = windStrength * windInfluence;
-            velocityModule.y = -fallSpeed;
-            velocityModule.z = 0f;
 
             // Configure size over lifetime (fade in/out for visual effect)
             sizeModule.enabled = true;
@@ -154,7 +97,7 @@ namespace Particles {
             }
 
             // Configure renderer for billboard particles (snowflakes)
-            var renderer = snowParticleSystem.GetComponent<ParticleSystemRenderer>();
+            var renderer = particleSystem.GetComponent<ParticleSystemRenderer>();
             if (renderer != null) {
                 renderer.renderMode = ParticleSystemRenderMode.Billboard;
                 renderer.alignment = ParticleSystemRenderSpace.World;
@@ -162,114 +105,25 @@ namespace Particles {
         }
 
         /// <summary>
-        /// Sets the snow intensity (0 = no snow, 1 = heavy blizzard)
-        /// </summary>
-        public void SetIntensity(float newIntensity) {
-            intensity = Mathf.Clamp01(newIntensity);
-            emissionModule.rateOverTime = baseEmissionRate * intensity;
-        }
-
-        /// <summary>
-        /// Sets the wind strength affecting snow (snow is very wind-sensitive)
-        /// </summary>
-        public void SetWindStrength(float strength) {
-            windStrength = strength;
-            if (windGustAmount <= 0f) {
-                velocityModule.x = windStrength * windInfluence;
-            }
-        }
-
-        /// <summary>
-        /// Sets wind gust parameters for dynamic wind changes
-        /// </summary>
-        public void SetWindGust(float gustAmount, float gustSpeed) {
-            windGustAmount = gustAmount;
-            windGustSpeed = gustSpeed;
-            windTimer = 0f;
-        }
-
-        /// <summary>
-        /// Sets the fall speed of snowflakes
-        /// </summary>
-        public void SetFallSpeed(float speed) {
-            fallSpeed = speed;
-            velocityModule.y = -fallSpeed;
-        }
-
-        /// <summary>
-        /// Sets the emission rate of snow particles
-        /// </summary>
-        public void SetEmissionRate(float rate) {
-            emissionRate = rate;
-            baseEmissionRate = rate;
-            emissionModule.rateOverTime = baseEmissionRate * intensity;
-        }
-
-        /// <summary>
-        /// Sets the color of the snow
-        /// </summary>
-        public void SetSnowColor(Color color) {
-            snowColor = color;
-            if (snowParticleSystem != null) {
-                mainModule.startColor = snowColor;
-            }
-        }
-
-        /// <summary>
-        /// Sets the spawn area dimensions
-        /// </summary>
-        public void SetSpawnArea(float width, float height) {
-            spawnWidth = width;
-            spawnHeight = height;
-            shapeModule.scale = new Vector3(spawnWidth, spawnHeight, 1f);
-        }
-
-        /// <summary>
-        /// Sets the snowflake size
-        /// </summary>
-        public void SetFlakeSize(float size) {
-            flakeSize = size;
-            if (snowParticleSystem != null) {
-                mainModule.startSize = new ParticleSystem.MinMaxCurve(flakeSize * (1f - flakeSizeVariation), flakeSize * (1f + flakeSizeVariation));
-            }
-        }
-
-        /// <summary>
-        /// Sets the fluttering effect of snowflakes
-        /// </summary>
-        public void SetFlutterAmount(float amount) {
-            flutterAmount = amount;
-        }
-
-        /// <summary>
         /// Starts the blizzard effect
         /// </summary>
         public void StartSnow() {
-            if (!snowParticleSystem.isPlaying) {
-                snowParticleSystem.Play();
-            }
+            StartEffect();
         }
 
         /// <summary>
         /// Stops the blizzard effect
         /// </summary>
         public void StopSnow() {
-            if (snowParticleSystem.isPlaying) {
-                snowParticleSystem.Stop();
-            }
+            StopEffect();
         }
-
-        /// <summary>
-        /// Checks if snow is currently falling
-        /// </summary>
-        public bool IsSnowing => snowParticleSystem.isPlaying && intensity > 0f;
 
 #if UNITY_EDITOR
         private void OnValidate() {
-            if (snowParticleSystem == null)
-                snowParticleSystem = GetComponent<ParticleSystem>();
+            if (particleSystem == null)
+                particleSystem = GetComponent<ParticleSystem>();
 
-            if (snowParticleSystem != null) {
+            if (particleSystem != null) {
                 InitializeParticleSystem();
             }
         }
--- Assets/Scripts/Particles/Rain.cs
+++ Assets/Scripts/Particles/Rain.cs
@@ -5,173 +5,41 @@ namespace Particles {
     /// Controls a rain particle system effect in the foreground of the scene.
     /// Attach this to a GameObject with a ParticleSystem component.
     /// </summary>
-    [RequireComponent(typeof(ParticleSystem))]
-    public class Rain : MonoBehaviour {
-        [Header("Rain Settings")] [Tooltip("Number of rain particles emitted per second")] [SerializeField]
-        private float emissionRate = 100f;
-
-        [Tooltip("Lifetime of each rain drop in seconds")] [SerializeField]
-        private float lifetime = 1.5f;
-
-        [Tooltip("Speed of falling rain")] [SerializeField]
-        private float fallSpeed = 20f;
-
-        [Tooltip("Width of the rain spawn area")] [SerializeField]
-        private float spawnWidth = 40f;
-
-        [Tooltip("Height of the rain spawn area")] [SerializeField]
-        private float spawnHeight = 2f;
-
-        [Tooltip("Length/stretch of rain drops")] [SerializeField]
+    public class Rain : Particles {
+        [Header("Rain Settings")]
+        [Tooltip("Length/stretch of rain drops")]
+        [SerializeField]
         private float dropLength = 100f;
 
-        [Tooltip("Width of rain drops")] [SerializeField]
+        [Tooltip("Width of rain drops")]
+        [SerializeField]
         private float dropWidth = 0.02f;
 
-        [Tooltip("Color of the rain")] [SerializeField]
+        [Tooltip("Color of the rain")]
+        [SerializeField]
         private Color rainColor = new Color(0.7f, 0.8f, 1f, 0.5f);
 
-        [Header("Wind Settings")] [Tooltip("Horizontal wind force affecting rain angle")] [SerializeField]
-        private float windStrength = 0f;
-
-        [Header("Intensity")]
-        [Range(0f, 1f)]
-        [Tooltip("Overall rain intensity (0 = no rain, 1 = full rain)")]
-        [SerializeField]
-        private float intensity = 1f;
-
-        private ParticleSystem rainParticleSystem;
-        private ParticleSystem.MainModule mainModule;
-        private ParticleSystem.EmissionModule emissionModule;
-        private ParticleSystem.ShapeModule shapeModule;
-        private ParticleSystem.VelocityOverLifetimeModule velocityModule;
-
-        private float baseEmissionRate;
-
-        private void Awake() {
-            rainParticleSystem = GetComponent<ParticleSystem>();
-            InitializeParticleSystem();
-        }
-
-        private void InitializeParticleSystem() {
-            // Cache modules
-            mainModule = rainParticleSystem.main;
-            emissionModule = rainParticleSystem.emission;
-            shapeModule = rainParticleSystem.shape;
-            velocityModule = rainParticleSystem.velocityOverLifetime;
-
-            // Configure main module
-            mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
-            mainModule.startLifetime = lifetime;
-            mainModule.startSpeed = 0f; // We control speed via velocity over lifetime
+        protected override void InitializeParticleSystem() {
+            base.InitializeParticleSystem();
+            
             mainModule.startSize = dropWidth; // Width of the rain drop (length controlled by renderer lengthScale)
             mainModule.startColor = rainColor;
-            mainModule.gravityModifier = 0f; // We handle gravity manually
-            mainModule.maxParticles = 10000;
-
-            // Configure emission
-            baseEmissionRate = emissionRate;
-            emissionModule.rateOverTime = emissionRate * intensity;
-
-            // Configure shape (box emitter above the camera)
-            shapeModule.shapeType = ParticleSystemShapeType.Box;
-            shapeModule.scale = new Vector3(spawnWidth, spawnHeight, 1f);
-
-            // Configure velocity for falling rain
-            velocityModule.enabled = true;
-            velocityModule.space = ParticleSystemSimulationSpace.World;
-            velocityModule.x = windStrength;
-            velocityModule.y = -fallSpeed;
-            velocityModule.z = 0f;
 
             // Configure renderer for stretched billboards (rain effect)
-            var renderer = rainParticleSystem.GetComponent<ParticleSystemRenderer>();
+            var renderer = particleSystem.GetComponent<ParticleSystemRenderer>();
             if (renderer != null) {
                 renderer.renderMode = ParticleSystemRenderMode.Stretch;
-                renderer.velocityScale = 0f; // Disable velocity-based stretching
-                renderer.lengthScale = dropLength; // Use dropLength to control visual length
+                renderer.velocityScale = 0f;
+                renderer.lengthScale = dropLength;
             }
         }
-
-        /// <summary>
-        /// Sets the rain intensity (0 = no rain, 1 = full rain)
-        /// </summary>
-        public void SetIntensity(float newIntensity) {
-            intensity = Mathf.Clamp01(newIntensity);
-            emissionModule.rateOverTime = baseEmissionRate * intensity;
-        }
-
-        /// <summary>
-        /// Sets the wind strength affecting rain angle
-        /// </summary>
-        public void SetWindStrength(float strength) {
-            windStrength = strength;
-            velocityModule.x = windStrength;
-        }
-
-        /// <summary>
-        /// Sets the fall speed of rain drops
-        /// </summary>
-        public void SetFallSpeed(float speed) {
-            fallSpeed = speed;
-            velocityModule.y = -fallSpeed;
-        }
-
-        /// <summary>
-        /// Sets the emission rate of rain particles
-        /// </summary>
-        public void SetEmissionRate(float rate) {
-            emissionRate = rate;
-            baseEmissionRate = rate;
-            emissionModule.rateOverTime = baseEmissionRate * intensity;
-        }
-
-        /// <summary>
-        /// Sets the color of the rain
-        /// </summary>
-        public void SetRainColor(Color color) {
-            rainColor = color;
-            mainModule.startColor = rainColor;
-        }
-
-        /// <summary>
-        /// Sets the spawn area dimensions
-        /// </summary>
-        public void SetSpawnArea(float width, float height) {
-            spawnWidth = width;
-            spawnHeight = height;
-            shapeModule.scale = new Vector3(spawnWidth, spawnHeight, 1f);
-        }
-
-        /// <summary>
-        /// Starts the rain effect
-        /// </summary>
-        public void StartRain() {
-            if (!rainParticleSystem.isPlaying) {
-                rainParticleSystem.Play();
-            }
-        }
-
-        /// <summary>
-        /// Stops the rain effect
-        /// </summary>
-        public void StopRain() {
-            if (rainParticleSystem.isPlaying) {
-                rainParticleSystem.Stop();
-            }
-        }
-
-        /// <summary>
-        /// Checks if rain is currently active
-        /// </summary>
-        public bool IsRaining => rainParticleSystem.isPlaying && intensity > 0f;
 
 #if UNITY_EDITOR
         private void OnValidate() {
-            if (rainParticleSystem == null)
-                rainParticleSystem = GetComponent<ParticleSystem>();
+            if (particleSystem == null)
+                particleSystem = GetComponent<ParticleSystem>();
 
-            if (rainParticleSystem != null && Application.isPlaying) {
+            if (particleSystem != null && Application.isPlaying) {
                 InitializeParticleSystem();
             }
         }
