6132955d3faf5351744122494c17ee2dff46c947
anjx123 <114792511+anjx123@users.noreply.github.com>
2026-01-05 22:52:56 -0800

    Added informative clashing icons  (#36)
    * First iteration of clash calculator
    
    Has a minor issue where the buff can change after the clash is resolved which will cause a re calculation when its not necessary
    
    * Replaced assets with proper ones
    
    * Clash Icon Cleanups + Tooltip helper
    
    * Battle Queue Icons Layering Fix and Prettying
    
    * Added Safety net and MR cleanup
    
    * Card reworks to remove insertion bug
    
    * Reduced font size slightly
    
    * Minor Adjustments

--- Assets/Scripts/UI Elements/BattleQueueIcons.cs
+++ Assets/Scripts/UI Elements/BattleQueueIcons.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using UI_Toolkit;
 using UnityEngine;
 
-public class BattleQueueIcons : DisplayableClass
+public interface IBattleQueueDisplayable
 {
+    void Emphasize();
+    void DeEmphasize();
+    GameObject GameObject { get;  }
+}
+
+public class BattleQueueIcons : DisplayableClass, IBattleQueueDisplayable
+{
+    public GameObject GameObject => gameObject;
     [SerializeField] SpriteRenderer targetRenderer;
     private SpriteRenderer iconRenderer;
-
-
     private int FadeSortingOrder => CombatFadeScreenHandler.Instance.FADE_SORTING_ORDER;
     private string FadeSortingLayer => CombatFadeScreenHandler.Instance.FADE_SORTING_LAYER;
 
     private void Awake()
     {
         iconRenderer = GetComponent<SpriteRenderer>();
+        Emphasize();
     }
 
-    public void Start()
+    public void Emphasize()
     {
         iconRenderer.sortingOrder = FadeSortingOrder + 4;
         iconRenderer.sortingLayerName = FadeSortingLayer;
         targetRenderer.sortingOrder = FadeSortingOrder + 5;
         targetRenderer.sortingLayerName = FadeSortingLayer;
+
     }
+
+    public void DeEmphasize()
+    {
+        iconRenderer.sortingOrder = FadeSortingOrder - 1;
+        targetRenderer.sortingOrder = FadeSortingOrder - 1;
+    }
+
+
+
     public void OnMouseDown()
     {
         if (ActionClass.Origin is PlayerClass && CombatManager.Instance.CanHighlight())
--- Assets/Scripts/UI Elements/ClashingBattleQueueIcon.cs
+++ Assets/Scripts/UI Elements/ClashingBattleQueueIcon.cs
@@ -1,24 +1,123 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ClashingBattleQueueIcon : MonoBehaviour
+#nullable enable
+public class ClashingBattleQueueIcon : MonoBehaviour, IBattleQueueDisplayable
 {
+    public GameObject GameObject => gameObject;
     [SerializeField]
-    private BattleQueueIcons leftClashingAction;
+    private BattleQueueIcons leftClashingAction = null!;
     [SerializeField]
-    private BattleQueueIcons rightClashingAction;
-    [SerializeField]
-    private SpriteRenderer swordsIcon;
+    private BattleQueueIcons rightClashingAction = null!;
+    [SerializeField] private SwordIcon swordIcon = null!;
 
-    public void Start()
-    {
-        swordsIcon.sortingLayerName = CombatFadeScreenHandler.Instance.FADE_SORTING_LAYER;
-        swordsIcon.sortingOrder = CombatFadeScreenHandler.Instance.FADE_SORTING_ORDER + 6;
-    }
-    public void renderClashingIcons(ActionClass leftClashingItem,  ActionClass rightClashingItem)
+    private static readonly ClashCalculator Calculator = new();
+    private ActionClass leftClashing = null!;
+    private ActionClass rightClashing = null!;
+
+
+    // Public initializer for this icon
+    public void RenderClashingIcons(ActionClass leftClashingItem,  ActionClass rightClashingItem)
     {
         leftClashingAction.RenderBQIcon(leftClashingItem);
         rightClashingAction.RenderBQIcon(rightClashingItem);
+
+        leftClashing = leftClashingItem;
+        rightClashing = rightClashingItem;
+        leftClashingItem.CardValuesUpdating += UpdateSwordIcon;
+        rightClashingItem.CardValuesUpdating += UpdateSwordIcon;
+
+        Emphasize();
+        UpdateSwordIcon(null);
     }
+
+    private void OnDisable()
+    {
+        leftClashing.CardValuesUpdating -= UpdateSwordIcon;
+        rightClashing.CardValuesUpdating -= UpdateSwordIcon;
+    }
+
+    public void Emphasize()
+    {
+        swordIcon.Emphasize();
+        leftClashingAction.Emphasize();
+        rightClashingAction.Emphasize();
+    }
+
+    public void DeEmphasize()
+    {
+        swordIcon.DeEmphasize();
+        leftClashingAction.DeEmphasize();
+        rightClashingAction.DeEmphasize();
+    }
+
+    private void UpdateSwordIcon(ActionClass? _) =>
+        swordIcon.SetClashState(Calculator.CompareRange(GetRange(leftClashing), GetRange(rightClashing)));
+
+    private (int, int) GetRange(ActionClass actionClass) => ((actionClass.GetRolledStats().RollFloor), (actionClass.GetRolledStats().RollCeiling));
+}
+
+public enum ClashResultType
+{
+    None = 0,
+    Dominating = 1,
+    Favored = 2,
+    Even = 3,
+    Unfavored = 4,
+    Futile = 5
+}
+
+public class ClashCalculator
+{
+    private readonly List<ClashBracket> _brackets = new()
+    {
+        new ClashBracket(0.2f, ClashResultType.Futile),
+        new ClashBracket(0.4f, ClashResultType.Unfavored),
+        new ClashBracket(0.6f, ClashResultType.Even),
+        new ClashBracket(0.8f, ClashResultType.Favored), 
+        new ClashBracket(1.0f, ClashResultType.Dominating)
+    };
+
+    [SerializeField] private bool _winsIncludeTies = true;
+
+    public ClashResultType CompareRange((int min, int max) leftRange, (int min, int max) rightRange)
+    {
+        float winChance = CalculateWinProbability(leftRange, rightRange, _winsIncludeTies);
+
+        foreach (var bracket in _brackets)
+        {
+            if (winChance <= bracket.Threshold)
+            {
+                return bracket.Result;
+            }
+        }
+        return ClashResultType.Dominating;
+    }
+
+    private float CalculateWinProbability((int min, int max) left, (int min, int max) right, bool includeTies)
+    {
+        float totalCombinations = (left.max - left.min + 1) * (right.max - right.min + 1);
+
+        if (totalCombinations <= 0) return 0f; 
+
+        int winningCombinations = 0;
+
+        for (int l = left.min; l <= left.max; l++)
+        {
+            int maxWinningRightRoll = includeTies ? l : l - 1;
+            int effectiveMaxR = Mathf.Min(maxWinningRightRoll, right.max);
+            if (effectiveMaxR < right.min) continue;
+            int winsForThisRoll = effectiveMaxR - right.min + 1;
+
+            winningCombinations += winsForThisRoll;
+        }
+
+        return winningCombinations / totalCombinations;
+    }
+
+
+    private record ClashBracket(float Threshold, ClashResultType Result) { }
 }
