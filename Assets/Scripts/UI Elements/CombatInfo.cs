7b4b5a31f6098572faaaced6fc3c810b8c907063
Sammy Kuang <31680668+sammy-kuang@users.noreply.github.com>
2026-01-15 00:20:09 -0800

    Update warning sprite,  add check on CombatInfo

--- Assets/Scripts/UI Elements/CombatInfo.cs
+++ Assets/Scripts/UI Elements/CombatInfo.cs
@@ -27,6 +27,7 @@ public class CombatInfo : MonoBehaviour
 
     private int FadeSortingOrder => CombatFadeScreenHandler.Instance.FADE_SORTING_ORDER;
     private string FadeSortingLayer => CombatFadeScreenHandler.Instance.FADE_SORTING_LAYER;
+    private bool BelongsToEnemy => transform.parent && transform.parent.GetComponent<EnemyClass>() != null;
 
     public void Awake()
     {
@@ -157,7 +158,7 @@ public class CombatInfo : MonoBehaviour
             combatIcon.transform.localPosition = new Vector3(0, startY - i * iconHeight, 0);
             combatIcon.GetComponent<CombatCardUI>().SetActionClass(combatCards[num - i - 1]); //Reverse the order of rendering 
             combatIcon.GetComponent<CombatCardUI>().DeEmphasize();
-            combatIcon.GetComponent<CombatCardUI>().RenderUnseenIndicator();
+            if (BelongsToEnemy) combatIcon.GetComponent<CombatCardUI>().RenderUnseenIndicator();
         }
     }
     private void UnrenderCombatIcons()
