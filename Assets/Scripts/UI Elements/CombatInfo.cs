91bef4ba1870eb5935032e5bfd19f1527a6d04a7
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2026-01-15 18:51:11 -0800

    Update Assets/Scripts/UI Elements/CombatInfo.cs

--- Assets/Scripts/UI Elements/CombatInfo.cs
+++ Assets/Scripts/UI Elements/CombatInfo.cs
@@ -27,7 +27,7 @@ public class CombatInfo : MonoBehaviour
 
     private int FadeSortingOrder => CombatFadeScreenHandler.Instance.FADE_SORTING_ORDER;
     private string FadeSortingLayer => CombatFadeScreenHandler.Instance.FADE_SORTING_LAYER;
-    private bool BelongsToEnemy => transform.parent && transform.parent.GetComponent<EnemyClass>() != null;
+private bool BelongsToEnemy => GetComponentInParent<EnemyClass>() != null;
 
     public void Awake()
     {
