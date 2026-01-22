e1e253484419b60f29098305c3bded27be987e95
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2026-01-22 00:51:08 -0800

    Struggle

---
+++ Assets/Scripts/Cards/FistCards/Struggle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Struggle : FistCards
+{
+    // Start is called before the first frame update
+    public override void Initialize()
+    {
+        lowerBound = 1;
+        upperBound = 1;
+        Speed = 1;
+
+        myName = "Struggle";
+        description = "You flail about as you are exhausted...";
+        CardType = CardType.MeleeAttack;
+        Renderer renderer = GetComponent<Renderer>();
+        base.Initialize();
+    }
+
+}
