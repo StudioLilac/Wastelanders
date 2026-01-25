9a662362d5b00a0ca372faf874f874130bf85d5c
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2026-01-25 11:47:34 -0800

    remove merge conflit markers

--- Assets/Scripts/Cards/ClasslessCards/Struggle.cs
+++ Assets/Scripts/Cards/ClasslessCards/Struggle.cs
@@ -2,23 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/Cards/FistCards/LowBlow.cs
-public class LowBlow : FistCards
-========
 public class Struggle : ClasslessCards
->>>>>>>> c8807af1 (Initial impl of struggle card):Assets/Scripts/Cards/ClasslessCards/Struggle.cs
 {
     public override void Initialize()
     {
-        lowerBound = 3;
-        upperBound = 3;
-        Speed = 5;
+        lowerBound = 1;
+        upperBound = 1;
+        Speed = 1;
         clashable = false;
 
-        myName = "Low Blow";
-        description = "This attack doesn't clash with enemy actions.";
+        myName = "Struggle";
+        description = "You flail about as you are exhausted...";
         CardType = CardType.MeleeAttack;
-        Renderer renderer = GetComponent<Renderer>();
         base.Initialize();
     }
 
--- Assets/Scripts/Cards/FistCards/LowBlow.cs
+++ Assets/Scripts/Cards/FistCards/LowBlow.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/Cards/FistCards/LowBlow.cs
 public class LowBlow : FistCards
-========
-public class Struggle : ClasslessCards
->>>>>>>> c8807af1 (Initial impl of struggle card):Assets/Scripts/Cards/ClasslessCards/Struggle.cs
 {
     public override void Initialize()
     {
@@ -18,7 +14,6 @@ public class Struggle : ClasslessCards
         myName = "Low Blow";
         description = "This attack doesn't clash with enemy actions.";
         CardType = CardType.MeleeAttack;
-        Renderer renderer = GetComponent<Renderer>();
         base.Initialize();
     }
 
