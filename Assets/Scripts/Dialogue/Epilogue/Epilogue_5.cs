385496403bcf4e4c7af96834a4b35dba0cd46458
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2026-03-10 22:32:04 -0700

    added all the cinema stuff

--- Assets/Scripts/Dialogue/Epilogue/Epilogue_5.cs
+++ Assets/Scripts/Dialogue/Epilogue/Epilogue_5.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using DialogueScripts;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace Dialogue.Epilogue {
     public class Epilogue_5 : MonoBehaviour {
         [SerializeField] private DialogueEntryInUnityEditor[] walkingDialogue;
+        [SerializeField] private DialogueEntryInUnityEditor[] blackDialogue;
+        [SerializeField] private DialogueEntryInUnityEditor[] labDialogue;
 
+        [SerializeField] private Image labBg;
+        [SerializeField] private SpriteFadeHandler blackFadeHandler;
+        [SerializeField] private UIFadeHandler labFadeHandler;
         private IEnumerator Start()
         {
             UIFadeScreenManager.Instance.SetDarkScreen();
+            labBg.gameObject.SetActive(false);
             yield return UIFadeScreenManager.Instance.FadeInLightScreen(2f);
             yield return new WaitForSeconds(1f);
             yield return DialogueBoxV2.Instance.Play(walkingDialogue.Into());
-            yield return UIFadeScreenManager.Instance.FadeInDarkScreen(2f);
+            yield return blackFadeHandler.FadeInDarkScreen(2f);
+            yield return DialogueBoxV2.Instance.Play(blackDialogue.Into());
+            labBg.gameObject.SetActive(true);
+            yield return labFadeHandler.FadeInDarkScreen(2f);
+            yield return DialogueBoxV2.Instance.Play(labDialogue.Into());
         }
     }
 }
