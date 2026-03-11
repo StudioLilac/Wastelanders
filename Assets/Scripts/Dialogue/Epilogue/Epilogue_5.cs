fb4104da75bc6c07686e174731f3f1503e62bbb1
anjx123 <114792511+anjx123@users.noreply.github.com>
2026-03-11 13:29:06 -0700

    Dialogue Adjustments that we will have to make back to the main script

--- Assets/Scripts/Dialogue/Epilogue/Epilogue_5.cs
+++ Assets/Scripts/Dialogue/Epilogue/Epilogue_5.cs
@@ -19,13 +19,14 @@ namespace Dialogue.Epilogue {
             labBg.gameObject.SetActive(false);
             yield return UIFadeScreenManager.Instance.FadeInLightScreen(2f);
             yield return new WaitForSeconds(1f);
+            StartCoroutine(blackFadeHandler.FadeToAlpha(0.7f, 1f));
             yield return DialogueBoxV2.Instance.Play(walkingDialogue.Into());
-            yield return blackFadeHandler.FadeInDarkScreen(2f);
+            yield return blackFadeHandler.FadeInDarkScreen(1f);
             yield return DialogueBoxV2.Instance.Play(blackDialogue.Into());
             labBg.gameObject.SetActive(true);
             yield return labFadeHandler.FadeInDarkScreen(2f);
             yield return DialogueBoxV2.Instance.Play(labDialogue.Into());
-            yield return UIFadeScreenManager.Instance.FadeInDarkScreen(2f);
+            yield return UIFadeScreenManager.Instance.FadeInDarkScreen(1.5f);
         }
     }
 }
