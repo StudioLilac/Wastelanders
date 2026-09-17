bd74eb19134d14fc7712705f1ef6d73afc09a687
anjx123 <114792511+anjx123@users.noreply.github.com>
2026-09-17 16:49:51 -0700

    Epilgoue Button Added thumbnails

--- Assets/Scripts/Epilogue/EpilogueButton.cs
+++ Assets/Scripts/Epilogue/EpilogueButton.cs
@@ -12,23 +12,41 @@ public class EpilogueButton : MonoBehaviour
     [SerializeField] private TMP_Text titleText;
     [SerializeField] private TMP_Text requirementText;
     [SerializeField] private Image thumbnail;
+    [SerializeField] private GameObject mask;
     private int completedBounties;
     private int neededBounties;
     private EpilogueSceneData sceneData;
 
+    [SerializeField] private Outline outline;
+    [SerializeField] private Vector2 defaultOutline;
+    [SerializeField] private Vector2 hoverOutline;
+    [SerializeField] private Color defaultOutlineColor = Color.white;
+    [SerializeField] private Color hoverOutlineColor;
+    [SerializeField] private Color defaultTextColor = Color.white;
+    [SerializeField] private Color hoverTextColor;
+    [SerializeField] private GameObject hover;
 
     public void Bind(EpilogueSceneData epilogueSceneData, EpilogueThumbnails thumbnails)
     {
         sceneData = epilogueSceneData;
         neededBounties = sceneData.BountyRequirement;
         thumbnail.sprite = epilogueSceneData.GetThumbnail(thumbnails);
-        completedBounties = PrincessFrogBounties.Values.Count(b => BountyManager.Instance.IsBountyCompleted(b));
+        completedBounties = BountyManager.Instance.GetBountyProgress();
         button.onClick.AddListener(() =>
         {
             GameStateManager.Instance.LoadScene(sceneData.SceneData.SceneName);
         });
         SetLocked(neededBounties > completedBounties);
         UpdateRequirementText();
+        SetHover(false);
+    }
+
+    public void SetHover(bool state) {
+        if (!button.enabled || !button.interactable) return;
+        hover.SetActive(state);
+        outline.effectDistance = state ? hoverOutline : defaultOutline;
+        outline.effectColor = state ? hoverOutlineColor : defaultOutlineColor;
+        titleText.color = state ? hoverTextColor : defaultTextColor;
     }
 
     private void SetTitle(string text)
@@ -40,11 +58,13 @@ public class EpilogueButton : MonoBehaviour
     {
         lockIndicator.enabled = state;
         button.interactable = !state;
+        mask.SetActive(!state);
         SetTitle(state ? "???" : sceneData.EpilogueTitle);
     }
 
     private void UpdateRequirementText()
     {
-        requirementText.SetText($"{completedBounties}/{neededBounties} bounties");
+        float completed = Mathf.Min(completedBounties, neededBounties);
+        requirementText.SetText($"{completed}/{neededBounties} bounties");
     }
 }
